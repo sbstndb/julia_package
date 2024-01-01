f844397ade536c1d3ae57897bbcfa994e02a3af8
Sébastien Dubois <sebastiendubois4@gmail.com>
2024-01-02 00:28:24 +0100

    add + try
    - add new packages
    - try test

--- download.jl
+++ download.jl
@@ -9,11 +9,43 @@ l_package = [
 "Makie",
 "PrecompileTools", 
 "JET",
+"StaticArrays",
+"SparseArrays",
+"krylovKit",
+"LinearSolve",
+"Kryov",
+"MKL",
+"KrylovMethods",
+"IDRsSolver",
+"LinearOperators",
+"AlgebraicMultigrid",
+"Multigrid", 
+"RandomizedPreconditioners",
+"ConjugateGradients",
+"ILUZero",
+"MLJ",
+"Mocha",
+"LoopVectorization",
+"Parameters"
 ]
 
+issue_package = []
 for package in l_package
-	Pkg.add(package)
+	println("Installing ", package, " ...")
+	try
+		Pkg.add(package)
+	catch 
+		println("Issue : cannot install ", package)
+		push!(issue_package, package)
+	end
 end
+
+number_issue = size(issue_package)
+println("*** issues : ", number_issue)
+if number_issue > 0 
+	println("cannot install packages : ", issue_package)
+end	
+
 
 
 
